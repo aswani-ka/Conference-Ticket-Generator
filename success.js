8e966a223ff1427d6a484b9471261873272fdd46
ASWANI K A <aswanika05@gmail.com>
2025-08-29 11:50:49 +0530

    chore: add values from the session storage to the success page

--- success.js
+++ success.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const userName = document.getElementById("user-name"); 
     const userImage = document.getElementById("user-image");
 
-    heading.innerHTML = `Congrats, <span class="highlight"> ${sessionStorage.getItem("name")}! </span> Your ticket is ready.`;
+    heading.innerHTML = `Congrats, <span class="highlight-heading"> ${sessionStorage.getItem("name")}! </span> Your ticket is ready.`;
     para.innerHTML = `We've emailed your ticket to <span class="highlight"> ${sessionStorage.getItem("email")} </span> and will send updates in the run up to the event.`;
     userImage.src = sessionStorage.getItem("image");
     userName.textContent = sessionStorage.getItem("name");
